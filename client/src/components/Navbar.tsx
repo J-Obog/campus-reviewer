02f1f122886a0c58b6d672ef8368e6b9d564905d
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-02-20 21:52:21 -0500

    Added avatar and dropdown menu to the navbar component.

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -14,6 +14,29 @@ const Navbar = () => {
           sign up
         </a>
       </div>
+      {/* <div className="dropdown dropdown-end">
+        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+          <div className="w-14 rounded-full md:w-32">
+            <img src="https://xsgames.co/randomusers/avatar.php?g=male" />
+          </div>
+        </label>
+        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+          <li>
+            <a>
+              Profile
+            </a>
+          </li>
+          <li>
+            <a>Settings</a>
+          </li>
+          <li>
+            <a>Saved</a>
+          </li>
+          <li>
+            <a>Logout</a>
+          </li>
+        </ul>
+      </div> */}
     </div>
   );
 };
