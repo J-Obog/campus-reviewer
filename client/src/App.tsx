4fd0818fe860dbea29e32c55c2d0cc868454b2cd
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-02-21 23:26:47 -0500

    Adding Search component.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,15 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Search from "./components/Search";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <main className="px-4 py-10 md:py-14">
+        <Search />
+      </main>
     </>
   );
 };
---
+++ client/src/components/SearchInput.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const SearchInput = () => {
+  return (
+    <>
+      <div className="form-control sm:ml-3 md:inline-block">
+        <div className="input-group w-full">
+          <label htmlFor="search" className="w-full sm:w-96 lg:w-139">
+            <input type="text" id="search" name="search" placeholder="Type college name..." className="input-bordered input w-full rounded-r-none" />
+          </label>
+          <button type="submit" className="btn-square btn">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SearchInput;
