1fec01c3d607f5ef35a0b71fb91bc656101113c8
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-02-22 00:17:53 -0500

    Created component class for the subtitles in the different pages.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Search from "./components/Search";
+import CardsContainer from "./components/CardsContainer";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Hero />
       <main className="px-4 py-10 md:py-14">
         <Search />
+        <CardsContainer />
       </main>
     </>
   );
---
+++ client/src/components/CardsContainer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CardsContainer = () => {
+  return (
+    <div className='my-6'>
+      <h2 className='page-subtitle'>Colleges</h2>
+      <div>
+        {/* cards will go here */}
+      </div>
+    </div>
+  );
+};
+
+export default CardsContainer;
