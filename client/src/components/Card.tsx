1a7c64b4a58cf873a2b3cd1b9daf6f5e341d3a94
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-02-22 19:21:03 -0500

    Adding Card and CardsContainer components.

---
+++ client/src/components/Card.tsx
@@ -0,0 +1,24 @@
+import { BiBookmark } from "react-icons/bi";
+
+const Card = () => {
+  return (
+    <div className="card bg-base-100 shadow-xl">
+      <figure>
+        <img src="https://picsum.photos/seed/picsum/500/300" alt="College picture" />
+      </figure>
+      <div className="card-body">
+        <h3 className="card-title">John Doe's University</h3>
+        
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nobis similique cupiditate sunt nihil possimus?</p>
+        <div className="card-actions items-end justify-between">
+          <a className="link-primary link capitalize">see more</a>
+          <button className="text-4xl">
+            <BiBookmark />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- client/src/components/CardsContainer.tsx
+++ client/src/components/CardsContainer.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import Card from "./Card";
 
 const CardsContainer = () => {
   return (
-    <div className='my-6'>
-      <h2 className='page-subtitle'>Colleges</h2>
-      <div>
-        {/* cards will go here */}
+    <div className="my-16">
+      <h2 className="page-subtitle">Colleges</h2>
+      <div className="my-12 grid grid-cols-1 justify-center gap-14 sm:grid-cols-2 sm:gap-x-6 md:gap-x-10 lg:mx-10 sm:mx-3 md:mx-6 2xl:mx-20 xl:mx-14 lg:grid-cols-3">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
     </div>
   );
