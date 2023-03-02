3f106906a808d9b543a06ccf235fa2cf4b876913
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-03-02 11:58:48 -0500

    Adding React Router to the project.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,14 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
