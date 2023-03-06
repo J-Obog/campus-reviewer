db4391ef5931ca0f5adebc8d5a252b6a8e363beb
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-03-06 11:51:02 -0500

    Made the Hero component dynamic.

---
+++ client/src/components/HeroRating.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeroRating = () => {
+  return (
+    <div>
+      Rating goes here...
+    </div>
+  );
+};
+
+export default HeroRating;
--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,11 +1,17 @@
 import Hero from "../components/Hero";
 import Search from "../components/Search";
 import CardsContainer from "../components/CardsContainer";
+import heroImg from "../assets/HeroImg.jpg"
 
 const LandingPage = () => {
   return (
     <>
-      <Hero />
+      <Hero 
+        image={heroImg} 
+        title="Campus Reviewer: rate college campuses with ease!"
+        description="Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi. Lorem ipsum dolor sit amet consectetur adipisicing elit."
+        showButton
+      />
       <main className="px-4 py-10 md:py-14">
         <Search />
         <CardsContainer />
