5c0514101e27f9ed72f92fd7111300c34fcd1b77
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-03-07 20:58:07 -0500

    Adding location page.

--- client/src/pages/CollegePage.tsx
+++ client/src/pages/CollegePage.tsx
@@ -7,19 +7,19 @@ const CollegePage = () => {
     <>
       <Hero 
         image={"https://image.cnbcfm.com/api/v1/image/106578950-1592283323893gettyimages-1212003765.jpeg?v=1592312973"} 
-        title={"Harvard University"}
-        description={"Harvard University is a private Ivy League research university in Cambridge, Massachusetts. Established in 1636 and named for its first benefactor, clergyman John Harvard, Harvard is the oldest institution of higher learning in the United States and among the most prestigious in the world."}
-        showButton={false}
-        rating={5.0}
+        title={"Harvard University"} 
+        description={"Harvard University is a private Ivy League research university in Cambridge, Massachusetts. Established in 1636 and named for its first benefactor, clergyman John Harvard, Harvard is the oldest institution of higher learning in the United States and among the most prestigious in the world."} 
+        showButton={false} 
+        rating={5.0} 
       />
       <main className="px-4 py-10 md:py-14">
         <div>
-          <div className="flex flex-col items-center gap-5 sm:flex-row sm:justify-center xl:justify-start">
+          <div className="flex flex-col items-center gap-5 sm:flex-row sm:justify-center lg:ml-5 xl:justify-start">
             <SaveButton />
             <button className="btn-primary btn-wide btn">add location</button>
           </div>
         </div>
-        <CardsContainer pageTitle="locations" itemType="locations"  />
+        <CardsContainer pageTitle="locations" itemType="locations" />
       </main>
     </>
   );
--- client/src/pages/LocationPage.tsx
+++ client/src/pages/LocationPage.tsx
@@ -8,13 +8,13 @@ const LocationPage = () => {
       <Hero
         image={"http://www.hoffarch.com/assets/SUNY-Whitman-overview.jpg"} 
         title={"Whitman Hall"}
-        description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris. Cras tincidunt lobortis feugiat vivamus at augue eget arcu. Justo donec enim diam vulputate ut pharetra sit amet aliquam. Imperdiet nulla malesuada pellentesque elit eget gravida. Morbi leo urna molestie at elementum eu facilisis. Fermentum leo vel orci porta non pulvinar neque laoreet suspendisse."}
+        description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris. Cras tincidunt lobortis feugiat vivamus at augue eget arcu. Justo donec enim diam vulputate ut pharetra sit amet aliquam."}
         showButton={false}
         rating={3.5}
       />
       <main className="px-4 py-10 md:py-14">
         <div>
-          <div className="flex flex-col items-center gap-5 sm:flex-row sm:justify-center xl:justify-start">
+          <div className="flex flex-col items-center gap-5 sm:flex-row sm:justify-center lg:ml-5 xl:justify-start">
             <SaveButton />
             <button className="btn-primary btn-wide btn">add review</button>
           </div>
