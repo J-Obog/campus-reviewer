9a3f56e3b99856c9d43807804af786519e2412ec
J-Obog <jobogbaimhe@gmail.com>
2023-03-23 19:53:52 -0400

    updating main function

--- server/main.ts
+++ server/main.ts
@@ -1,10 +1,13 @@
 import { DataSource } from "typeorm"
-import { CollegeModel } from "./db/postgres/college" 
+import { CollegeModel, CollegeStorePg } from "./db/postgres/college" 
 import { getConfig } from "./config"; 
+import { ExpressServer } from "./api/express";
+import { CollegeResourceImpl } from "./api/resources/college";
 
 async function main() {
     const config = getConfig(); 
     
+    // database connection
     const conn = new DataSource({
         type: "postgres",
         host: config.postgresConfig.host,
@@ -15,12 +18,21 @@ async function main() {
         entities: [CollegeModel],
     })    
 
-    // connect to database
     try {
         await conn.initialize(); 
     } catch(err) {
         console.error(err); 
     }
+
+    // db stores
+    const collegeStore = new CollegeStorePg(conn); 
+
+    // resources
+    const collegeResource = new CollegeResourceImpl(collegeStore); 
+
+    // app server
+    const server = new ExpressServer(collegeResource); 
+    server.run(config.expressConfig.port); 
 }
 
 main();
