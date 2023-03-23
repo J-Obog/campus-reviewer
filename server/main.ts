21e40993a14763a0c597b85bf1fa5cb9a4c3df14
J-Obog <jobogbaimhe@gmail.com>
2023-03-23 14:53:30 -0400

    create db conn in main app function

--- server/main.ts
+++ server/main.ts
@@ -1,7 +1,19 @@
-import * as dotenv from 'dotenv'
+import { DataSource } from "typeorm"
+import { CollegePgEntity } from "./db/postgres/college-store" 
+import { getConfig } from "./config"; 
 
-async function main() {
-    console.log("Hello world!");
+function main() {
+    const config = getConfig(); 
+    
+    const conn = new DataSource({
+        type: "postgres",
+        host: config.postgresConfig.host,
+        username: config.postgresConfig.username,
+        password: config.postgresConfig.password,
+        port: config.postgresConfig.port,
+        database: config.postgresConfig.database,
+        entities: [CollegePgEntity],
+    })
 }
 
 main();
