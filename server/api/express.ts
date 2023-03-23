fa0800e321b266dd7f7f8db05e00edcc5efd5b1b
J-Obog <jobogbaimhe@gmail.com>
2023-03-23 19:43:28 -0400

    add req currying for handling express requests

--- server/api/express.ts
+++ server/api/express.ts
@@ -1,11 +1,31 @@
 import { Server } from "./server";
-import express, { Router, Express } from "express";
+import express from "express";
+import { RestResponse, RestRequest } from "./rest";
+import { CollegeResource } from "./resource";
+
 
 export class ExpressServer implements Server {
-    app: Express; 
+    app: express.Express; 
 
-    constructor() {
+    constructor(collegeResource: CollegeResource) {
         this.app = express();
+        this.app.get("/college", collegeResource.getAllColleges);
+        this.app.get("/college/:id", collegeResource.getCollege);
+    }
+
+    _request_handler(restFn: (req: RestRequest) => RestResponse): express.RequestHandler {
+        return (req: express.Request, res: express.Response) => {
+            const restReq: RestRequest = {
+                url: req.url, 
+                urlParams: req.params,
+                queryParams: req.query,
+                meta: {},
+                body: req.body,
+            } 
+            
+            const fnResponse = restFn(restReq);
+            res.status(fnResponse.status).json(fnResponse.data);
+        }
     }
 
     run(port: number): void {
