1e8288286df10ce8b15f3384f39944e1899f30bd
J-Obog <jobogbaimhe@gmail.com>
2023-03-23 16:06:03 -0400

    fixing syntax error in migrations file

--- server/db/migrations.sql
+++ server/db/migrations.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS colleges (
     "id" VARCHAR PRIMARY KEY,
     "name" VARCHAR(255) NOT NULL,
     "rating" FLOAT NOT NULL,
-    "photo" VARCHAR NULL, 
+    "photo" VARCHAR NULL
 );
