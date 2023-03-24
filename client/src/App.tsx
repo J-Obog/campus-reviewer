6df0cb41a208fe4cc38debdd3ccb29f360bbe579
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-03-24 12:32:09 -0400

    Adding nested routing in App.tsx and adding the SignUpPage.tsx component.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,21 @@
 import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import LandingPage from "./pages/LandingPage";
 import CollegePage from "./pages/CollegePage";
 import LocationPage from "./pages/LocationPage";
-import Footer from "./components/Footer";
+import PageLayout from "./layouts/PageLayout";
+import SignUpPage from "./pages/SignUpPage";
 
 const App = () => {
   return (
     <>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/college" element={<CollegePage />} />
-        <Route path="/location" element={<LocationPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route element={<PageLayout />}>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/college" element={<CollegePage />} />
+          <Route path="/location" element={<LocationPage />} />
+        </Route>
       </Routes>
-      <Footer />
     </>
   );
 };
---
+++ client/src/pages/SignUpPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUpPage = () => {
+  return (
+    <div>
+      Sign Up Page
+    </div>
+  );
+};
+
+export default SignUpPage;
