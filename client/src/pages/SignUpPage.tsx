e282f56fa43b8a2bfafa92cf784eda92ae6f17d3
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-03-24 12:41:20 -0400

    Adding prettier and prettier config.

--- client/src/pages/SignUpPage.tsx
+++ client/src/pages/SignUpPage.tsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React from "react";
 
 const SignUpPage = () => {
   return (
-    <div>
-      Sign Up Page
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+        <div className="card w-full max-w-sm flex-shrink-0 bg-base-100 shadow-2xl">
+          <div className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="email"
+                className="input-bordered input"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="text"
+                placeholder="password"
+                className="input-bordered input"
+              />
+              <label className="label">
+                <a href="#" className="link-hover label-text-alt link">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn-primary btn">Login</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
