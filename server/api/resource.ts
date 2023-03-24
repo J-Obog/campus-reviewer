50cc6e307cdebdea914e95c0b5ebe9a8a0e80e33
J-Obog <jobogbaimhe@gmail.com>
2023-03-23 22:47:25 -0400

    adding location resource method impls

--- server/api/resource.ts
+++ server/api/resource.ts
@@ -4,3 +4,8 @@ export interface CollegeResource {
     getAllColleges(req: RestRequest): Promise<RestResponse>; 
     getCollege(req: RestRequest): Promise<RestResponse>;
 }
+
+export interface LocationResource {
+    getLocation(req: RestRequest): Promise<RestResponse>; 
+    getLocationsByCollege(req: RestRequest): Promise<RestResponse>;
+}
