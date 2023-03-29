23c4978256285786a1b68d7631a3cb1a7efa220b
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-03-29 10:44:39 -0400

    Updated background color of all the pages.

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -2,8 +2,11 @@ import Hero from "../components/Hero";
 import Search from "../components/Search";
 import CardsContainer from "../components/CardsContainer";
 import heroImg from "../assets/HeroImg.jpg";
+import { useAppSelector } from "../redux/store";
 
 const LandingPage = () => {
+  const themeValue = useAppSelector((state) => state.theme.value);
+
   return (
     <>
       <Hero
@@ -13,7 +16,9 @@ const LandingPage = () => {
         showButton
         rating={0}
       />
-      <main className="px-4 py-10 md:py-14">
+      <main
+        className={`${themeValue === "winter" && "bg-custom-light"} px-4 py-10 md:py-14 ${themeValue === "night" && "bg-custom-dark"}`}
+      >
         <Search />
         <CardsContainer pageTitle="colleges" itemType="college" />
       </main>
