6b3a1e509af0c989b38d4166ff1a88afdcf36b5a
J-Obog <jobogbaimhe@gmail.com>
2023-03-30 21:20:55 -0400

    add base review store with read only methods

--- server/db/store.ts
+++ server/db/store.ts
@@ -1,4 +1,4 @@
-import { College, Location } from "./schema"; 
+import { College, Location, Review } from "./schema"; 
 
 export interface CollegeStore {
     getAllColleges(): Promise<College[]>; 
@@ -8,4 +8,9 @@ export interface CollegeStore {
 export interface LocationStore {
     getLocation(id: string): Promise<Location | null>; 
     getLocationsByCollege(collegeId: string): Promise<Location[]>;  
+}
+
+export interface ReviewStore {
+    getReview(id: string): Promise<Review | null>; 
+    getReviewByLocation(locationId: string): Promise<Review[]>;  
 }
